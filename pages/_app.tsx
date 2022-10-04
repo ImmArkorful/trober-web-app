571c361c03f5c0cb31098749d2d422cbc3774248
ImmArkorful <e.arkorful3@gmail.com>
2022-10-04 23:14:29 +0000

    feat: footer and animation library added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, []);
   return <Component {...pageProps} />;
 }
 
