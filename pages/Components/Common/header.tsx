2af56478e512001edee395e2b21c054054da0427
Emmanuel Arkorful <66488171+ImmArkorful@users.noreply.github.com>
2022-10-04 21:38:53 +0000

    feat: responsive header component created (#2)

---
+++ pages/Components/Common/header.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { BiMenu } from 'react-icons/bi';
+
+const Header: NextPage = () => {
+  const [menuActive, setMenuActive] = useState(true);
+  const [menuStyle, setMenuStyle] = useState('opacity-0 top-[-400px]');
+
+  const toggleMenuActive = () => {
+    setMenuActive(!menuActive);
+  };
+  const handleToggleMenu = () => {
+    menuActive
+      ? setMenuStyle('top-[80px] opacity-100')
+      : setMenuStyle('opacity-0 top-[-400px]');
+    toggleMenuActive();
+  };
+  return (
+    <header>
+      <nav className="p-5 bg-primary shadow md:flex md:items-center md:justify-between">
+        <div className="flex justify-between items-center ">
+          <img
+            alt=""
+            className="h-10 inline"
+            src="https://tailwindcss.com/_next/static/media/social-square.b622e290e82093c36cca57092ffe494f.jpg"
+          />
+          <BiMenu
+            role="button"
+            className="text-3xl cursor-pointer mx-2 md:hidden block"
+            onClick={() => {
+              handleToggleMenu();
+            }}
+            onKeyDown={() => {
+              handleToggleMenu();
+            }}
+            tabIndex={0}
+            color="white"
+          />
+        </div>
+
+        <ul
+          className={`${menuStyle} bg-primary font-semibold text-white md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 transition-all ease-in duration-500`}
+        >
+          <li className="mx-4 my-6 md:my-0">
+            <Link href="/">
+              <a className="text-xl hover:text-cyan-500 duration-500">
+                About Us
+              </a>
+            </Link>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <Link href="/">
+              <a href="/" className="text-xl hover:text-cyan-500 duration-500">
+                Services
+              </a>
+            </Link>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <Link href="/">
+              <a href="/" className="text-xl hover:text-cyan-500 duration-500">
+                Partners
+              </a>
+            </Link>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <Link href="/">
+              <a href="/" className="text-xl hover:text-cyan-500 duration-500">
+                Media
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <div> </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      primary: '#0F1A37',
+      white: '#FFF',
+    },
     extend: {},
   },
   plugins: [],
