7c9c16ff7886ad2dd13f52ebbfcea08da18af325
JoelEdemAmenuvor <67765884+JhayStark@users.noreply.github.com>
2023-02-01 22:59:09 +0000

    chore: contact us form (#16)

--- pages/Components/Common/contactUs.tsx
+++ pages/Components/Common/contactUs.tsx
@@ -1,8 +1,27 @@
+import { useForm } from 'react-hook-form';
+import axiosInstance from '../../../utils/axiosInstance';
+
 interface ContactUsProps {
   backgroundLg: string;
   backgroundSm: string;
 }
 const ContactUs = ({ backgroundLg, backgroundSm }: ContactUsProps) => {
+  const defaultValues = {
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+    message: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues,
+  });
   return (
     <div
       className={`w-full my-32  md:min-h-[70%] 2xl:min-h-[90%] flex flex-col items-center justify-between py-14 2xl:py-44 
@@ -12,31 +31,84 @@ const ContactUs = ({ backgroundLg, backgroundSm }: ContactUsProps) => {
       <span className="text-3xl text-white underline decoration-underlinecolor">
         Contact Us
       </span>
-      <form className="flex flex-col items-center justify-center w-full">
+      <form
+        className="flex flex-col items-center justify-center w-full"
+        onSubmit={handleSubmit(async (data) => {
+          clearErrors();
+          try {
+            await axiosInstance.post('/contactUs/', {
+              ...data,
+            });
+            reset(defaultValues);
+          } catch (e: any) {
+            if (e instanceof Error) {
+              e.message;
+            }
+          }
+        })}
+      >
         <div className="flex flex-col items-center w-full px-10 mt-16 lg:mt-24 md:flex-row md:justify-evenly mb-102xl:px-44">
           <div className="flex flex-col items-center justify-center w-full ">
             <input
               type="text"
-              className="mb-4 md:mb-10 w-[100%] md:w-80 2xl:w-96 h-12 rounded-md border-gray-300 border-2 bg-gray-200 p-2 opacity-70 placeholder-gray-600"
-              placeholder="Name"
+              className={`mb-4 md:mb-10 w-[100%] md:w-80 2xl:w-96 h-12 rounded-md  border-2 bg-gray-200 p-2 opacity-70 ${
+                errors.fullName
+                  ? `placeholder-red-500 border-red-500`
+                  : `placeholder-gray-600 border-gray-300`
+              }`}
+              placeholder={`${
+                errors.fullName ? `${errors.fullName?.message}` : `Name`
+              }`}
+              {...register('fullName', {
+                required: 'Please Enter your full name',
+              })}
+            />
+
+            <input
+              type="text"
+              className={`mb-4 md:mb-10 w-[100%] md:w-80 2xl:w-96 h-12 rounded-md  border-2 bg-gray-200 p-2 opacity-70 ${
+                errors.phoneNumber
+                  ? `placeholder-red-500 border-red-500`
+                  : `placeholder-gray-600 border-gray-300`
+              }`}
+              placeholder={`${
+                errors.phoneNumber
+                  ? `${errors.phoneNumber?.message}`
+                  : `Phone Number`
+              }`}
+              {...register('phoneNumber', {
+                required: 'Please Enter your phone Number',
+              })}
             />
             <input
               type="text"
-              className="mb-4 md:mb-10 w-[100%] md:w-80 2xl:w-96 h-12 rounded-md border-gray-300 border-2 bg-gray-200 p-2 opacity-70 placeholder-gray-600"
-              placeholder="Phone Number"
-            />
-            <input
-              type="text"
-              className=" w-[100%] md:w-80 h-12 rounded-md 2xl:w-96 border-gray-300 border-2 bg-gray-200 p-2 opacity-70 placeholder-gray-600"
-              placeholder="Email"
+              className={`mb-4 md:mb-10 w-[100%] md:w-80 2xl:w-96 h-12 rounded-md  border-2 bg-gray-200 p-2 opacity-70 ${
+                errors.email
+                  ? `placeholder-red-500 border-red-500`
+                  : `placeholder-gray-600 border-gray-300`
+              }`}
+              placeholder={`${
+                errors.email ? `${errors.email?.message}` : `Email`
+              }`}
+              {...register('email', {
+                required: 'Please Enter your email',
+              })}
             />
           </div>
           <div className="flex w-full mt-4 md:mt-0 md:items-end">
             <textarea
-              name="message"
               id="message"
-              className="h-40 w-[100%] md:w-80 border-gray-300 rounded-md bg-gray-200 p-2 opacity-70 placeholder-gray-600 2xl:w-96 resize-none"
-              placeholder="Message"
+              className={`h-40 w-[100%] md:w-80  rounded-md bg-gray-200 p-2 opacity-70 ${
+                errors.message
+                  ? `placeholder-red-500 border-red-500 border-2`
+                  : `placeholder-gray-600 border-gray-300`
+              } 2xl:w-96 resize-none`}
+              placeholder={`${
+                errors.message ? `${errors.message?.message}` : `Message`
+              }`}
+              {...register('message', {
+                required: 'Please Enter your message',
+              })}
             />
           </div>
         </div>
