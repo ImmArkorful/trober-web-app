953c8516e32a9cbf59c6ecb385cdfedd29dfaafc
JhayStark <jaamen5@gmail.com>
2023-06-18 18:27:44 +0000

    updated about component text and rebuilt partnerflyer component while updating partner pages text

--- pages/partners/fleet-managers.tsx
+++ pages/partners/fleet-managers.tsx
@@ -18,30 +18,67 @@ const FleetManagers = () => {
           route=""
         />
       </div>
-      <div className="flex flex-col items-center justify-center mt-24 mx-11 md:text-3xl">
+      <div className="flex flex-col items-center justify-center mt-24 md:px-20 2xl:px-44 mx-11 md:text-3xl">
+        <p className="w-full text-left">Welcome Fleet Managers! </p>
         <p className="mb-8">
-          Earn extra income by listing your bus on our platform. We can assist
-          in getting a driver for your bus even if you do not have one. Earn
-          extra income by listing your bus on our platform. We can assist in
-          getting a driver for your bus even if you do not have one.
+          Are you looking for an efficient and cost-effective solution for
+          managing your fleet? Look no further than our fleet management
+          software (to be released soon) and ridesharing platform!
         </p>
         <p className="mb-8">
-          Earn extra income by listing your bus on our platform. We can assist
-          in getting a driver for your bus even if you do not have one. Earn
-          extra income by listing your bus on our platform. We can assist in
-          getting a driver for your bus even if you do not have one.
+          Our platform offers a range of features that are designed to help you
+          manage your fleet with ease. From tracking vehicles to scheduling
+          rides, our platform makes it easy to streamline your operations and
+          save time and money.
+        </p>
+        <p>With our fleet managment software underway, you can:</p>
+        <p className="mb-8">
+          Track your vechiles: Our software allows you to track the location and
+          status of vehicles in real-time. This makes it easy to monitor your
+          fleet and ensure that everything is running smoothly.
         </p>
         <p className="mb-8">
-          Earn extra income by listing your bus on our platform. We can assist
-          in getting a driver for your bus even if you do not have one. Earn
-          extra income by listing your bus on our platform. We can assist in
-          getting a driver for your bus even if you do not have one.
+          Schedule maintenance: Our software allows you to schedule routine
+          maintenance for your vehicles, ensuring that they are always in top
+          condition. This can help you reduce repair costs and prolong the life
+          of your vehicles.
         </p>
         <p className="mb-8">
-          Earn extra income by listing your bus on our platform. We can assist
-          in getting a driver for your bus even if you do not have one. Earn
-          extra income by listing your bus on our platform. We can assist in
-          getting a driver for your bus even if you do not have one.
+          Manage expenses: Our software allows you to track expenses related to
+          your fleet, including fuel costs, maintenance expenses, and more. This
+          can help you identify areas where you can save money and reduce your
+          overall expenses.
+        </p>
+        <p className="mb-8">
+          Analyze data: Our software provides detailed analytics and reporting
+          that allow you to analyze data related to your fleet. This can help
+          you identify trends, track performance, and make data-driven
+          decisions.
+        </p>
+        <p className="mb-8">
+          In addition to our fleet management software, we also offer a
+          ridesharing platform that allows you to book and manage rides for your
+          employees or customers. Our platform offers a range of benefits,
+          including: Cost savings: By using our ridesharing platform, you can
+          reduce your transportation costs and save money on your overall
+          operations.
+        </p>
+        <p className="mb-8">
+          Convenience: Our platform makes it easy to schedule and manage rides
+          for your employees or customers. With just a few clicks, you can book
+          rides and track their progress in real-time.
+        </p>
+        <p className="mb-8">
+          Safety and security: Our platform ensures that all rides are conducted
+          in a safe and secure manner. We vet all of our drivers and ensure that
+          they have a clean driving record and are experienced and professional.
+        </p>
+        <p className="mb-8">
+          We are committed to providing the best possible experience for our
+          customers. We believe that our platform can help you manage your fleet
+          more efficiently and save money on your operations. Contact us today
+          to learn more about our services and how we can help your
+          organization.
         </p>
       </div>
       <ContactUs
