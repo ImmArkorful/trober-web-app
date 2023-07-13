0ac08fb8652622e60d08faaf487f494930840be8
ImmArkorful <e.arkorful3@gmail.com>
2023-07-13 22:36:56 +0200

    FB pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import { Analytics, getAnalytics } from 'firebase/analytics';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import firebaseConfig from '../utils/contants';
+import Script from 'next/script';
 
 const app = initializeApp(firebaseConfig);
 
@@ -43,6 +44,18 @@ const HeadAndTags = () => {
         property="og:image"
         content="https://troberapp.com/trobericon.jpg"
       />
+      <Script>
+        {`!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '341079580930713');
+        fbq('track', 'PageView');`}
+      </Script>
     </Head>
   );
 };
